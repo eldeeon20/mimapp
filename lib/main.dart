6304324e183d4b9577c9b26c30008deeb996eb66
eduardo-bertey <eduardo-bertey@users.noreply.github.com>
2026-08-21 19:36:01 -0300

    Revert: restore main.dart to working version (d926738)

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,16 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:pr_app/src/rust/frb_generated.dart';
 
 import 'gui/engine_shell.dart';
 import 'services/colab_service.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-const _notifChannelId = 'pr_app_channel';
-const _notifChannelName = 'pr_app';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  } catch (_) {}
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   MediaKit.ensureInitialized();
-
-  // await _initNotifications();
-  await _initBackgroundService();
-
-  try {
-    await RustLib.init();
-  } catch (e) {
-    debugPrint('Rust init error: $e');
-  }
-
-  // try {
-  //   await ColabService().init();
-  // } catch (e) {
-  //   debugPrint('ColabService init error: $e');
-  // }
-
+  await RustLib.init();
+  await ColabService().init();
   runApp(const PrApp());
-}
-
-Future<void> _initNotifications() async {
-  try {
-    const androidSettings = AndroidInitializationSettings('ic_bg_service_small');
-    const iosSettings = DarwinInitializationSettings(
-      requestAlertPermission: false,
-      requestBadgePermission: false,
-      requestSoundPermission: false,
-    );
-    const initSettings = InitializationSettings(
-      android: androidSettings,
-      iOS: iosSettings,
-    );
-    await flutterLocalNotificationsPlugin.initialize(
-      settings: initSettings,
-    );
-
-    final androidPlugin = flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>();
-    if (androidPlugin != null) {
-      await androidPlugin.createNotificationChannel(
-        const AndroidNotificationChannel(
-          _notifChannelId,
-          _notifChannelName,
-          importance: Importance.high,
-        ),
-      );
-      await androidPlugin.requestNotificationsPermission();
-    }
-  } catch (e) {
-    debugPrint('Notificaciones init error: $e');
-  }
-}
-
-Future<void> _initBackgroundService() async {
-  try {
-    final service = FlutterBackgroundService();
-    await service.configure(
-      androidConfiguration: AndroidConfiguration(
-        onStart: _onServiceStart,
-        autoStart: true,
-        isForegroundMode: true,
-        notificationChannelId: _notifChannelId,
-        initialNotificationTitle: 'pr_app',
-        initialNotificationContent: 'Servicio activo',
-        foregroundServiceNotificationId: 888,
-      ),
-      iosConfiguration: IosConfiguration(
-        autoStart: false,
-        onForeground: _onServiceStart,
-      ),
-    );
-  } catch (e) {
-    debugPrint('BackgroundService init error: $e');
-  }
-}
-
-@pragma('vm:entry-point')
-Future<void> _onServiceStart(ServiceInstance service) async {
-  service.on('stop').listen((_) {
-    service.stopSelf();
-  });
 }
