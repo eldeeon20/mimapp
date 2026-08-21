bfb9c4df46bbcbde0ecdc64b268a66b850323ff7
eduardo-bertey <eduardo-bertey@users.noreply.github.com>
2026-08-21 18:51:35 -0300

    Fix: import dart:ui for DartPluginRegistrant + add MIGRATION.md

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
